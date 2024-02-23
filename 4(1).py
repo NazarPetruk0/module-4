16d41c19989c3aaae3bb8ca7583778b3a08eb34e
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-02-23 15:00:44 +0200

    Update 4(1).py

--- 4(1).py
+++ 4(1).py
@@ -12,13 +12,10 @@ def total_salary(path):
                 raise ValueError("Файл порожній. Немає даних про розробників.")
             average_salary = total_salary / num_developers
             return total_salary, average_salary
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Файл '{path}' не знайдено.")
-    except Exception as e:
-        raise Exception(f"Помилка при обробці файлу: {str(e)}")
-
+    except Exception:
+        pass
 try:
     total, average = total_salary("path/to/salary_file.txt")
     print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-except Exception as e:
-    print(f"Помилка: {str(e)}")
+except Exception:
+    pass
